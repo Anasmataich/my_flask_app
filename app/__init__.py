a00051bd5adebc71f382a2db163212040902d910
Anas <anasmataich6@gmail.com>
2025-10-23 20:29:25 +0200

    first commit4

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
+    @app.route("/", methods=['GET', 'POST'])  # <--- إضافة POST هنا
     def index():
+        if request.method == 'POST':
+            name = request.form.get('name')
+            return f"Hello, {name}!"
         return "<h2>Flask Form</h2>"
 
     return app
